4415346f8603eaa64e8f273fb67f84aa801f6c29
Daniel Rasmuson <dan123911@gmail.com>
2016-05-12 21:07:53 -0700

    some more fancy stuff

--- src/blocks/add-block.js
+++ src/blocks/add-block.js
@@ -3,6 +3,16 @@ import './rx-block.js';
 var proto = Object.create(HTMLElement.prototype);
 
 proto.createdCallback = function() {
+  var codeString = `
+    Rx.Observable.create((observer)=>{
+      observer.next(15);
+      observer.next(30);
+      observer.next(45);
+      observer.next(65);
+      observer.complete();
+    });
+  `
+
   const root = this.createShadowRoot();
   root.innerHTML = `
     <button onclick="this.parentNode.addBlock()">Add Block</button>
@@ -12,6 +22,8 @@ proto.createdCallback = function() {
   root.addBlock = ()=>{
     blocks.appendChild(document.createElement('rx-block'));
   }
+
+
 };
 
 var AddBlockButton = document.registerElement('add-block-button', {prototype: proto});
--- src/rx-demo.js
+++ src/rx-demo.js
@@ -5,24 +5,17 @@ var proto = Object.create(HTMLElement.prototype);
 proto.createdCallback = function() {
   const root = this.createShadowRoot();
 
-  var codeString = `
-    Rx.Observable.create((observer)=>{
-      observer.next(15);
-      observer.next(30);
-      observer.next(45);
-      observer.next(65);
-      observer.complete();
-    });
-  `
+  function assignLineChartNewObservable(){
+    lineChart.setAttribute('data-observable', codeString);
+  }
 
   root.innerHTML = `
     <add-block-button></add-block-button>
     <line-chart></line-chart>
   `;
-
-
   const lineChart = root.querySelector('line-chart');
-  lineChart.setAttribute('data-observable', codeString);
+
+
 };
 document.registerElement('rx-demo', {prototype: proto});
 
