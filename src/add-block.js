db462bf4ffe44d5cb275ebffc9b88638f274882f
Daniel Rasmuson <dan123911@gmail.com>
2016-05-12 18:15:41 -0700

    trying

--- src/add-block.js
+++ src/add-block.js
@@ -1,6 +1,15 @@
-/**
- * A not-very-useful inline element
- */
-Polymer({
-    is: 'my-element'
-});
+var proto = Object.create(HTMLElement.prototype);
+
+function addBlock(){
+  alert('add block')
+}
+
+proto.createdCallback = function() {
+  var shadow = this.createShadowRoot();
+  shadow.innerHTML = `
+    <button click="addBlock()">Add Block</button>
+  `;
+};
+
+
+var AddBlockButton = document.registerElement('add-block-button', {prototype: proto});
--- src/app.js
+++ src/app.js
@@ -1,16 +1,1 @@
-var proto = Object.create(HTMLElement.prototype);
-
-proto.createdCallback = function() {
-  this.addEventListener('click', ()=>{
-    alert('click')
-  })
-
-  var shadow = this.createShadowRoot();
-  shadow.innerHTML = `
-    <div>
-      Hello How are you?
-    </div>
-  `;
-};
-
-var XFoo = document.registerElement('x-foo', {prototype: proto});
+import 'add-block'; 
