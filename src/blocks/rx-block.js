e5f3a50a257ab5c1344df7857df9f7b9b9924aa1
Daniel Rasmuson <dan123911@gmail.com>
2016-05-12 21:36:08 -0700

    added default value

--- src/blocks/rx-block.js
+++ src/blocks/rx-block.js
@@ -1,16 +1,23 @@
 import * as _ from 'lodash';
 var proto = Object.create(HTMLElement.prototype);
+var root;
 proto.createdCallback = function() {
   const colors = ['#E53935', '#D81B60', '#8E24AA', '#5E35B1', '#3949AB', '#1E88E5', '#039BE5', '#00ACC1'];
   const color = colors[_.random(colors.length - 1)];
-  const root = this.createShadowRoot();
+  const defaultText ='.map((result)=>{return result;})';
 
+  root = this.createShadowRoot();
   root.innerHTML = `
-    <div style="background-color: ${color}; width: 300px; color: white;" contenteditable="true">
-      .map((result)=>{
-        return result;
-      })
+    <div style="background-color: ${color}; width: 300px; color: white;" contenteditable="true" class="text">
+      ${defaultText}
     </div>
   `;
 };
+
+proto.attributeChangedCallback = (attrName, oldVal, newVal)=>{
+  if (attrName === 'value'){
+    root.querySelector('.text').textContent = newVal;
+  }
+}
+
 document.registerElement('rx-block', {prototype: proto});
